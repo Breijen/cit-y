2322c2e23f7ab4b3b4c9c3a440a9b3aaca2906b3
Breijen <bryan.laanvd@gmail.com>
2024-07-29 16:51:48 +0200

    Auth Fix
    - Fixed blocked user when not logged in

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,18 +8,26 @@
 
     <div class="min-h-screen">
         <div class="space-y-2 w-full max-w-4xl mx-auto p-4 bg-content_bg rounded-3xl border border-divider">
-        @if($posts->count() > 0)
-            @foreach($posts->sortByDesc('created_at') as $post)
-                @if(!auth()->user()->isBlockedBy($post->user))
-                    @include("components.posts._post")
-                @endif
-            @endforeach
-            @auth
+        @auth
+            @if($posts->count() > 0)
+                @foreach($posts->sortByDesc('created_at') as $post)
+                    @if(!auth()->user()->isBlockedBy($post->user))
+                        @include("components.posts._post", ['post' => $post])
+                    @endif
+                @endforeach
                 @include("components.posts._create_comment_modal")
                 @include("components.posts._edit_post_modal")
                 @include("components.posts._delete_post_modal")
-            @endauth
-        @endif
+            @endif
+        @endauth    
+
+        @guest
+            @if($posts->count() > 0)
+                @foreach($posts->sortByDesc('created_at') as $post)
+                    @include("components.posts._post", ['post' => $post])
+                @endforeach
+            @endif
+        @endguest
         </div>
     </div>
 
