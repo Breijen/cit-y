c7f53e20db56d1e08decec1fd6a479c5ea863e23
Breijen <bryan.laanvd@gmail.com>
2024-08-21 13:50:28 +0200

    Correct Pinned Post

--- resources/views/components/profile/_pinned.blade.php
+++ resources/views/components/profile/_pinned.blade.php
@@ -1,8 +1,8 @@
 <div class="h-auto">
     <div class="space-y-2 w-full max-w-4xl mx-auto p-4 bg-content_bg rounded-lg border border-divider">
         <p class="pl-2 text-xs text-placeholder">pinned post</p>
-        @if(auth()->user()->pinnedPost)
-            @include("components.posts._post", ['post' => auth()->user()->pinnedPost])
+        @if($user->pinnedPost)
+            @include("components.posts._post", ['post' => $user->pinnedPost])
         @endif
     </div>
 </div>
